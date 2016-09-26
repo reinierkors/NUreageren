7fdb373f95cb639b8a6817f5a9912d5ea3e7fc3c
xDiglett <xdiglett@gmail.com>
2016-09-26 11:38:32 +0200

    Fixed count update, added auto-updates.

--- NUreageren.nl.user.js
+++ NUreageren.nl.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         NUreageren.nl
 // @namespace    https://nureageren.nl/
-// @version      1.0
+// @version      1.1
+// @updateURL    https://github.com/reinierkors/NUreageren/raw/master/NUreageren.nl.user.js
 // @description  Deze plugin zorgt ervoor dat jij weer kunt reageren op nu.nl.
 // @author       NUreageren.nl
 // @match        *://www.nu.nl/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js
 // @grant        none
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
 // ==/UserScript==
 
 (function() {
@@ -17,20 +18,24 @@
         var social = $('.social-buttons');
 
         if (url != '/' && social.length) {
-            httpGetAsync("https://nureageren.nl/api/count/" + url.split('/')[2]);
             social.prepend('<li><a href="https://nureageren.nl' + url + '" rel="nofollow" class="tracksocial" target="_blank"><span id="count" class="counter">-</span><div class="social-button" style="background-color: #eaf0fa; border: none;"><div class="button-content"><i class="fc fc-nujij"></i><span class="socialtext">Reageer</span></div></div></a><div class="clear"></div></li>');
+            httpGetAsync("https://nureageren.nl/api/count/" + url.split('/')[2]);
         }
     });
-
-    // http://stackoverflow.com/questions/247483/http-get-request-in-javascript
-    function httpGetAsync(theUrl)
-    {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                $('#count').text(xmlHttp.responseText);
-        };
-        xmlHttp.open("GET", theUrl, true);
-        xmlHttp.send(null);
-    }
 })();
+
+// http://stackoverflow.com/questions/247483/http-get-request-in-javascript
+function httpGetAsync(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        console.log(xmlHttp.readyState);
+        console.log(xmlHttp.status);
+
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            $('span#count').text(xmlHttp.responseText);
+        }
+    };
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
