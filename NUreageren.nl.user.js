2ae2f42489268ac7e2a0b72fd0a5e950d3934aaf
xDiglett <xdiglett@gmail.com>
2016-11-01 20:56:18 +0100

    jQuery dependency verwijderd. Support voor toekomstige sites.

--- NUreageren.nl.user.js
+++ NUreageren.nl.user.js
@@ -1,38 +1,70 @@
 // ==UserScript==
 // @name         NUreageren.nl
 // @namespace    https://nureageren.nl/
-// @version      1.11
+// @version      2.00
 // @updateURL    https://github.com/reinierkors/NUreageren/raw/master/NUreageren.nl.user.js
 // @description  Deze plugin zorgt ervoor dat jij weer kunt reageren op nu.nl.
 // @author       NUreageren.nl
 // @match        *://www.nu.nl/*
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    $(function() {
-        var url = window.location.pathname;
-        var social = $('.social-buttons');
+    var path = window.location.pathname;
+    var site = window.location.hostname;
+    var sites = ["www.nu.nl"];
 
-        if (url != '/' && social.length) {
-            social.prepend('<li><a href="https://nureageren.nl' + url + '" rel="nofollow" class="tracksocial" target="_blank"><span id="count" class="counter">-</span><div class="social-button" style="background-color: #eaf0fa; border: none;"><div class="button-content"><i class="fc fc-nujij"></i><span class="socialtext">Reageer</span></div></div></a><div class="clear"></div></li>');
-            httpGetAsync("https://nureageren.nl/api/count/" + url.split('/')[2]);
+    if(!include(sites, site) || path == '/') {
+        return;
+    }
+
+    switch(site) {
+        case 'www.nu.nl':
+            nu_nl();
+            break;
+        default:
+            console.log("Deze site word nog niet ondersteund.");
+            break;
+    }
+
+    function nu_nl() {
+        var buttons = document.getElementsByClassName('social-buttons');
+        if(buttons.length) {
+            var newButton = document.createElement('li');
+            newButton.innerHTML = '<a href="https://nureageren.nl' + path + '" rel="nofollow"' +
+                'class="tracksocial" target="_blank"><span class="nureageren_count counter">-</span>' +
+                '<div class="social-button" style="background-color: #eaf0fa; border: none;">' +
+                '<div class="button-content"><i class="fc fc-nujij"></i><span class="socialtext">' +
+                'Reageer</span></div></div></a>';
+
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].insertBefore(newButton, buttons[i].firstChild);
+            }
+
+            setCount("nureageren_count");
         }
-    });
-})();
+    }
 
-// http://stackoverflow.com/questions/247483/http-get-request-in-javascript
-function httpGetAsync(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
+    function setCount(elem) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            $('span#count').text(xmlHttp.responseText);
+            setText(elem, xmlHttp.responseText);
+        }};
+        xmlHttp.open("GET", "https://nureageren.nl/api/count/" + path.split('/')[2], true);
+        xmlHttp.send(null);
+    }
+
+    function include(arr, obj) {
+        return (arr.indexOf(obj) != -1);
+    }
+
+    function setText(id, newCount) {
+        var count = document.getElementsByClassName(id);
+        for (var i = 0; i < count.length; i++) {
+            count[i].innerHTML = newCount;
         }
-    };
-    xmlHttp.open("GET", theUrl, true);
-    xmlHttp.send(null);
-}
+    }
+})();
